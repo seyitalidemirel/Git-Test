2f0fa0c9fff0ef59bccdddec785803d590f8da1c
seyitalidemirel <132512897+seyitalidemirel@users.noreply.github.com>
2023-05-04 01:23:15 +0300

    Update TestClas.java

--- src/main/java/TestClas.java
+++ src/main/java/TestClas.java
@@ -1,5 +1,7 @@
 public class TestClas {
     public static void main(String[] args) {
         System.out.println("GittenMerhaba");
+        System.out.println("GithobdanMerhaba");
+        
     }
 }
